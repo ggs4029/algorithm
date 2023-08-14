7ed3b0a07935914d6019797191f1e53fd2787e90
ggs4029 <ggs65429@gmail.com>
2023-08-14 19:47:05 +0900

    백준 25083번 풀이

---
+++ Baekjoon/pr/src/pr/pr25083.java
@@ -0,0 +1,13 @@
+// 백준 25083번
+// 새싹 출력하기
+
+public class pr25083 {
+	public static void main(String[] args) {
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.print("      |");
+	}
+}
