ac808d60a4a1009b24ebe9a42b99ffb4aff2895e
ggs4029 <ggs65429@gmail.com>
2024-09-29 15:22:05 +0900

    백준 7287번 풀이

---
+++ Baekjoon/pr/src/pr/pr7287.java
@@ -0,0 +1,12 @@
+// 백준 7287번
+// 자신의 맞은 문제수와 아이디 출력하기
+// 방법---------------------
+// 맞은 문제수랑 아이디 출력
+
+public class pr7287 {
+	public static void main(String[] args) {
+		//출력
+		System.out.println(203);
+		System.out.print("ggs4029");
+	}
+}
