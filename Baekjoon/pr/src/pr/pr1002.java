52ded594f2edf12b2f59c7704e1426434a28ddd6
ggs4029 <ggs65429@gmail.com>
2025-03-01 20:16:27 +0900

    백준 1002번 수정

--- Baekjoon/pr/src/pr/pr1002.java
+++ Baekjoon/pr/src/pr/pr1002.java
@@ -19,7 +19,7 @@ public class pr1002 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine());
-		for (int i = 0; i < t; i++) {
+		for(int i = 0; i < t; i++) {
 			// 입력
 			String[] input = br.readLine().split(" ");
 			int x1 = Integer.parseInt(input[0]);
