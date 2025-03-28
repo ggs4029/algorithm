081a506fd69331dab1451024c5ba5f428ac73cab
ggs4029 <ggs65429@gmail.com>
2025-03-28 19:55:17 +0900

    백준 10189번 풀이

---
+++ Baekjoon/pr/src/pr/pr10189.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class pr10189 {
+	public static void main(String[] args) throws IOException {
+		// 출력
+		System.out.println("#  # #### #### #  #");
+		System.out.println("#### #  # #  # # #");
+		System.out.println("#### #  # #  # # #");
+		System.out.print("#  # #### #### #  #");
+	}
+}
