5eea263021a8d58000e9ee0d995520ed372397be
ggs4029 <ggs65429@gmail.com>
2025-05-03 20:30:58 +0900

    백준 32384번 풀이

---
+++ Baekjoon/pr/src/pr/pr32384.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class pr32384 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 입력
+		int n = Integer.parseInt(br.readLine());
+		// 출력
+		for(int i=0; i<n; i++) {
+			System.out.print("LoveisKoreaUniversity ");
+		}
+	}
+}
