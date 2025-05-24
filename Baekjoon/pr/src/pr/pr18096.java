7f28566d6b8fd227bc0edb0c7331ddba0fefb55e
ggs4029 <ggs65429@gmail.com>
2025-05-24 17:47:52 +0900

    백준 18096번 풀이

---
+++ Baekjoon/pr/src/pr/pr18096.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class pr18096 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 입력
+		int n = Integer.parseInt(br.readLine());
+		// 출력
+		System.out.print(1);
+	}
+}
