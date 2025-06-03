abd1ff63dfbd5ba3aa5ff2ff484a7a417c739332
ggs4029 <ggs65429@gmail.com>
2025-06-03 20:14:29 +0900

    백준 21598번 풀이

---
+++ Baekjoon/pr/src/pr/pr21598.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class pr21598 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		// 입력
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		for(int i=0; i<n; i++)
+			sb.append("SciComLove").append("\n");
+		// 출력
+		System.out.print(sb.toString().trim());
+	}
+}
