45e29f6b51f2ba62c712b29c7507ccf99d719ea1
ggs4029 <ggs65429@gmail.com>
2025-06-23 18:52:54 +0900

    백준 10817번 풀이

---
+++ Baekjoon/pr/src/pr/pr10817.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class pr10817 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 입력
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] num = new int[3];
+		num[0] = Integer.parseInt(st.nextToken());
+		num[1] = Integer.parseInt(st.nextToken());
+		num[2] = Integer.parseInt(st.nextToken());
+		// 정렬
+		Arrays.sort(num);
+		// 출력
+		System.out.print(num[1]);
+	}
+}
