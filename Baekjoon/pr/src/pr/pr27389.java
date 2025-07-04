8165e3d9f19e405be3e34cec8af4db18bfd24c72
ggs4029 <ggs65429@gmail.com>
2025-07-04 10:44:40 +0900

    백준 27389번 풀이

---
+++ Baekjoon/pr/src/pr/pr27389.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class pr27389 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 입력
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int x = Integer.parseInt(st.nextToken());
+		// 출력
+		System.out.print((double)x/4);
+	}
+}
