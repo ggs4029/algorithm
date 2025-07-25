29ecb364763ecc681e25873f78637a48f2a32a6f
ggs4029 <ggs65429@gmail.com>
2025-07-25 17:36:15 +0900

    백준 31606번 풀이

---
+++ Baekjoon/pr/src/pr/pr31606.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class pr31606 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 입력
+		int x = Integer.parseInt(br.readLine());
+		int y = Integer.parseInt(br.readLine());
+		// 출력
+		System.out.print(x+y+3);
+	}
+}
