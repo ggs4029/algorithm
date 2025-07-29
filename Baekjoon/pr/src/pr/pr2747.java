7555962e80c85202236fc6e7d15070aad54aa130
ggs4029 <ggs65429@gmail.com>
2025-07-29 18:31:43 +0900

    백준 2747번 풀이

---
+++ Baekjoon/pr/src/pr/pr2747.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class pr2747 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 입력
+		int n = Integer.parseInt(br.readLine());
+		// 피보나치 수 구하기
+		long[] fibo = new long[45+1];
+		fibo[0] = 0; fibo[1] = 1;
+		for(int i=2; i<fibo.length; i++)
+			fibo[i] = (fibo[i-1] + fibo[i-2]);
+		// 출력
+		System.out.print(fibo[n]);
+	}
+}
