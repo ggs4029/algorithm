39f6ef7dfb58aeec377bf761082927496a93d578
ggs4029 <ggs65429@gmail.com>
2025-08-10 16:48:18 +0900

    백준 33169번 풀이

---
+++ Baekjoon/pr/src/pr/pr33169.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class pr33169 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 입력
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		// 출력
+		System.out.print(a*1000 + b*10000);
+	}
+}
