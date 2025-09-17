117cecc1859472b53342b847e6c1e01c9baa3ac7
ggs4029 <ggs65429@gmail.com>
2025-09-17 11:06:04 +0900

    백준 13866번 풀이

---
+++ Baekjoon/pr/src/pr/pr13866.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class pr13866 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 입력
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		// 출력
+		System.out.print(Math.abs((a + d) - (b + c)));
+	}
+}
