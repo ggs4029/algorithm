6d755c005be85930071fa9fa680e0a8d4d95e2df
ggs4029 <ggs65429@gmail.com>
2025-09-19 16:39:58 +0900

    백준 34411번 풀이

---
+++ Baekjoon/pr/src/pr/pr34411.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class pr34411 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 입력
+		int m = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		int y = Integer.parseInt(br.readLine());
+		// 출력
+		System.out.print(m * n);
+	}
+}
