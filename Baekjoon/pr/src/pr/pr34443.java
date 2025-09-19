4173fa62063c133ed9b4d123f35a2c396157e10b
ggs4029 <ggs65429@gmail.com>
2025-09-19 16:47:35 +0900

    백준 34443번 풀이

---
+++ Baekjoon/pr/src/pr/pr34443.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class pr34443 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 입력
+		int n = Integer.parseInt(br.readLine());
+		int c = Integer.parseInt(br.readLine());
+		int p = Integer.parseInt(br.readLine());
+		// 출력
+		System.out.print(n * p);
+	}
+}
