fb2fc0a92540be46f473890cabcaaeb637745736
ggs4029 <ggs65429@gmail.com>
2025-09-23 12:29:22 +0900

    백준 14935번 풀이

---
+++ Baekjoon/pr/src/pr/pr14935.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class pr14935 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 입력
+		String input = br.readLine();
+		// 출력
+		System.out.print("FA");
+	}
+}
