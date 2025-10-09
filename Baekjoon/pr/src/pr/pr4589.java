9360c4d5763ad92de058619fe27c738df47b09bf
ggs4029 <ggs65429@gmail.com>
2025-10-09 16:55:19 +0900

    백준 4589번 풀이

---
+++ Baekjoon/pr/src/pr/pr4589.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class pr4589 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Gnomes:");
+		// 입력
+		int n = Integer.parseInt(br.readLine());
+		for(int i=0; i<n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			// 출력
+			if(a>=b && b>=c || a<=b && b<=c)
+				System.out.println("Ordered");
+			else
+				System.out.println("Unordered");
+		}
+	}
+}
