dc1dd7176c40d005c567c6434f52558f20230613
ggs4029 <ggs65429@gmail.com>
2025-10-30 14:11:26 +0900

    백준 18408번 풀이

---
+++ Baekjoon/pr/src/pr/pr18408.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class pr18408 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 입력
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		// 출력
+		if(a==b || a==c)
+			System.out.print(a);
+		else
+			System.out.print(b);
+	}
+}
