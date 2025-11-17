d5f0b025edc93f9c7128bc4aac97927aa4a685ca
ggs4029 <ggs65429@gmail.com>
2025-11-17 12:37:24 +0900

    백준 31962번 풀이

---
+++ Baekjoon/pr/src/pr/pr31962.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class pr31962 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 입력
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int x = Integer.parseInt(st.nextToken());
+		int output = -1;
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int t = Integer.parseInt(st.nextToken());
+			// 지각하는지 계산
+			if(s+t <= x) {
+				if(s > output)
+					output = s;
+			}
+		}
+		// 출력
+		System.out.print(output);
+	}
+}
