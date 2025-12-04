56efb30087e47e0eda96d7b46cbf5dba0776c3cb
ggs4029 <ggs65429@gmail.com>
2025-12-04 18:24:02 +0900

    백준 34823번 풀이

---
+++ Baekjoon/pr/src/pr/pr34823.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class pr34823 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 입력
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int y = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int p = Integer.parseInt(st.nextToken());
+		// 출력
+		System.out.print(Math.min(c/2, Math.min(y, p)));
+	}
+}
